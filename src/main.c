07bbc8e443c38e6a231c5e620eb059b307f0c733
Maksonaldo <shiliekov@mail.ru>
2019-04-26 12:58:09 +0700

    Changes files main.c and travis

--- src/main.c
+++ src/main.c
@@ -9,7 +9,7 @@
 int main()
 {
     FILE* myfile;
-    myfile = fopen("myfile.txt", "r");
+    myfile = fopen("../myfile.txt", "r");
     if (myfile == NULL) {
         printf("No file\n");
         return 1;
