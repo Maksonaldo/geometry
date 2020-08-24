f7f108cd9a533e47ea5316f0215afda2bbab952a
Maksonaldo <shiliekov@mail.ru>
2020-08-24 16:14:12 +0700

    19

--- src/geometry.c
+++ src/geometry.c
@@ -1,88 +1,68 @@
+#include "geometry.h"
+#include <ctype.h>
+#include <math.h>
 #include <stdio.h>
-#include "palindrom.h"
-//проверка на палиндром----------------------------------------
-int check(char str[]) {
-  int i, length;
-     length = strlen(str);
-  if (length == 1)
+#include <stdlib.h>
+#include <string.h>
+#define TRIANGLE 1
+#define CIRCLE 2
+
+int Parser(Figure* new, char* A)
+{
+      char B[256];
+    int i = 0;
+    while (A[i] >= 'a' && A[i] <= 'z') {
+        B[i] = A[i];
+        i++;
+    }
+    B[i] = '\0';
+    if (!(strcmp(B, "triangle"))) {
+        new->type = TRIANGLE;
+    } else if (!(strcmp(B, "circle"))) {
+        new->type = CIRCLE;
+    } else {
+        printf("Unknown type\n");
+        return 1;
+    }
+    char* end;
+    end = A;
+    i = -1;
+    new->coordinates = (double*)malloc(sizeof(double) * 100);
+    if (new->coordinates == NULL) {
+        printf("Alloceted error");
+        return 1;
+    }
+    while (*A) {
+        new->coordinates[i] = strtod(A, &end);
+        A = end;
+        i++;
+        while (!(isdigit(*A) || *A == '-' || *A == '+') && *A) {
+            A++;
+        }
+    }
+    new->size = i;
     return 0;
-  for (i = 0; i < length; i++)
-    if (str[i] != str[(length - 1) - i])
-      return 0;
-  return 1;
 }
-//конец проверки ----------------------------------------
 
-//преобразование заглавных букв в строчные------------------------------------
-void transformation_A_a() {
-
-  FILE *fp = fopen("file.txt", "r+");
-  if (fp == NULL) {
-    fprintf(stderr, "Невозможно открыть файл\n");
-    return 0;
-  }
-  freopen("test.txt", "a", stdout);
-  char c;
-  while ((c = fgetc(fp)) != EOF) {
-    if (c >= 'A' && c <= 'Z')
-      putchar(c - 'A' + 'a');
-
-    else
-      putchar(c);
-  }
-  fclose(stdout);
+void S_And_P_Circle(double* S, double* P, Figure* new)
+{
+    double r = new->coordinates[2];
+    *S = M_PI * (r * r);
+    *P = 2 * M_PI * r;
 }
-//----------------------------------------------------
 
-//удаление знаков препинания----------------------------------------------
-void deleting_punctuation_marks(char *str) {
-
-  freopen("text.txt", "a", stdout);
-
-  FILE *file;
-
-  if ((file = fopen("test.txt", "r")) == NULL) {
-    puts("File not found!");
-    return 1;
-  }
-
-
-  char *mas;
- 
-  mas = calloc(sizeof(char), 1000);
-  fgets(str, 80, file);
-  int i, j;
-  for (i = 0, j = 0; str[i]; ++i) {
-    if (str[i] == '.' || str[i] == ':' || str[i] == ',' || str[i] == '!' ||
-        str[i] == '?' || str[i] == ';' || str[i] == '-' || str[i] == '<' ||
-        str[i] == '>')
-      continue;
-    mas[j] = str[i];
-    j++;
-  }
-
-  if (check(mas))
-    printf("\n%s  \n", mas);
-  else
-    printf("\n%s \n", mas);
-
-  fclose(stdout);
-}
-//---------------------------------------------------------------------------
-//сравнение слов, поиск паландрома
-void word_comparison(char *ptr) {
-  freopen("text.txt", "a", stdout);
-
-  FILE *f;
-  if ((f = fopen("text.txt", "r")) == NULL) {
-    puts("File not found!");
-    return 1;
-  }
-  while (!feof(f)) {
-    fscanf(f, "%s", ptr);
-    if (check(ptr) == 1)
-      printf(" \n Slovo: %s Palindrome \n", ptr);
-    else
-      printf("\n SLOVO: %s Not palindrome \n", ptr);
-  }
+void S_And_P_Triangle(double* S, double* P, Figure* new)
+{
+    double a
+            = sqrt(pow((new->coordinates[2] - new->coordinates[0]), 2.0)
+                   + pow((new->coordinates[3] - new->coordinates[1]), 2.0));
+    double b
+            = sqrt(pow((new->coordinates[4] - new->coordinates[2]), 2.0)
+                   + pow((new->coordinates[5] - new->coordinates[3]), 2.0));
+    double c
+            = sqrt(pow((new->coordinates[0] - new->coordinates[4]), 2.0)
+                   + pow((new->coordinates[1] - new->coordinates[5]), 2.0));
+    *P = a + b + c;
+    double p = (a + b + c) / 2;
+    *S = sqrt(p * (p - a) * (p - b) * (p - c));
 }
--- src/geometry.h
+++ src/geometry.h
@@ -1,8 +1,14 @@
-#include <locale.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-int check(char str[]);
-void transformation_A_a();
-void deleting_punctuation_marks(char *str);
-void word_comparison(char *ptr);
+#ifndef GEOMETRY_H
+#define GEOMETRY_H
+
+typedef struct {
+    int type;
+    double* coordinates;
+    int size;
+} Figure;
+
+int Parser(Figure* new, char* A);
+void S_And_P_Circle(double* S, double* P, Figure* new);
+void S_And_P_Triangle(double* S, double* P, Figure* new);
+
+#endif
